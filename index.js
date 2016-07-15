6fedade786ed2cb262466969f40418454d77db9f
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2016-07-15 10:43:58 +0900

    use native Promise instead of pinkie-promise
    [breaking change]

--- index.js
+++ index.js
@@ -7,20 +7,19 @@
 var inspect = require('util').inspect;
 
 var parseFile = require('bplist-parser').parseFile;
-var PinkiePromise = require('pinkie-promise');
 
 var ERR = 'Expected a path to the binary plist (.bplist) file';
 
 module.exports = function readBplist(filePath) {
   if (typeof filePath !== 'string') {
-    return PinkiePromise.reject(new TypeError(inspect(filePath) + ' is not a string. ' + ERR + '.'));
+    return Promise.reject(new TypeError(inspect(filePath) + ' is not a string. ' + ERR + '.'));
   }
 
   if (filePath === '') {
-    return PinkiePromise.reject(new Error(ERR + ', but received an empty string.'));
+    return Promise.reject(new Error(ERR + ', but received an empty string.'));
   }
 
-  return new PinkiePromise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
     parseFile(filePath, function(err, results) {
       if (err) {
         reject(err);
