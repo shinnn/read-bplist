e9d0d0e565061bfeb4017658efc0e77bd6e978bb
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2016-07-19 10:43:58 +0900

    use const, template strings and arrow functions
    [breaking change]

--- index.js
+++ index.js
@@ -4,23 +4,23 @@
 */
 'use strict';
 
-var inspect = require('util').inspect;
+const inspect = require('util').inspect;
 
-var parseFile = require('bplist-parser').parseFile;
+const parseFile = require('bplist-parser').parseFile;
 
-var ERR = 'Expected a path to the binary plist (.bplist) file';
+const ERR = 'Expected a path to the binary plist (.bplist) file';
 
 module.exports = function readBplist(filePath) {
   if (typeof filePath !== 'string') {
-    return Promise.reject(new TypeError(inspect(filePath) + ' is not a string. ' + ERR + '.'));
+    return Promise.reject(new TypeError(`${inspect(filePath)} is not a string. ${ERR}.`));
   }
 
   if (filePath === '') {
-    return Promise.reject(new Error(ERR + ', but received an empty string.'));
+    return Promise.reject(new Error(`${ERR}, but received an empty string.`));
   }
 
-  return new Promise(function(resolve, reject) {
-    parseFile(filePath, function(err, results) {
+  return new Promise((resolve, reject) => {
+    parseFile(filePath, (err, results) => {
       if (err) {
         reject(err);
         return;
